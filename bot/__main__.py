3a6ec25db0f8bd4f91b223e5ede85f8ef57396eb
unknown <2006liuchang@163.com>
2021-12-02 23:34:11 +0800

    1

--- bot/__main__.py
+++ bot/__main__.py
@@ -9,7 +9,8 @@ from bot.helper.utils import get_formatted_chat
 @app.on_message(filters.chat(from_chats) & filters.incoming)
 def work(client, message):
     caption = None
-    msg = None
+    LOG.info('new mesg word')
+    msg = message.text.html[1:]
     if remove_strings:
       for string in remove_strings:
         if message.media and not message.poll:
@@ -22,7 +23,8 @@ def work(client, message):
           if caption:
             message.copy(chat, caption=caption)
           elif msg:
-            app.send_message(chat, msg, parse_mode="html")
+            LOG.info('new mesg word')
+            app.send_message(chat, msg[1:], parse_mode="html")
           else:
             message.copy(chat)
       except Exception as e:
@@ -30,39 +32,51 @@ def work(client, message):
     else:
       try:
         for chat in to_chats:
-          if caption:
-            message.copy(chat, caption=caption)
-          elif msg:
-            app.send_message(chat, msg)
-          else:
-            message.copy(chat)
+          try:
+            if caption:
+              LOG.info(" if caption:")
+              message.copy(chat, caption=caption)
+            elif msg:
+              LOG.info(chat)
+              LOG.info(msg)
+              app.send_message(chat, msg)
+            else:
+              message.copy(chat)
+          except Exception as e:
+            print(e)
       except Exception as e:
         LOG.error(e)
 
-@app.on_message(filters.user(sudo_users) & filters.command(["fwd", "forward"]), group=1)
+@app.on_message(filters.user(sudo_users) & filters.command(["fwd", "forward", "docs"]), group=1)
 def forward(app, message):
-    if len(message.command) > 1:
-      chat_id = get_formatted_chat(message.command[1], app)
-      if chat_id:
-        try:
-          offset_id = 0
-          limit = 0
-          if len(message.command) > 2:
-            limit = int(message.command[2])
-          if len(message.command) > 3:
-            offset_id = int(message.command[3])
-          for msg in app.iter_history(chat_id, limit=limit, offset_id=offset_id):
-            msg.copy(message.chat.id)
-            sleep(random.randint(1, 10))
-        except Exception as e:
-          message.reply_text(f"```{e}```")
-      else:
-        reply = message.reply_text("```Invalid Chat Identifier. Give me a chat id, username or message link.```")
-        sleep(5)
-        reply.delete()
+    LOG.info(message.command)
+    if message.command.__contains__('docs'):
+      LOG.info('contains__')
+      chat = get_formatted_chat(message.chat.id, app)
+      app.send_message(chat, 'docs.cheersland.org')
     else:
-      reply = message.reply_text("```Invalid Command ! Use /fwd {ChatID} {limit} {FirstMessageID}```")
-      sleep(20)
-      reply.delete()
+      if len(message.command) > 1:
+        chat_id = get_formatted_chat(message.command[1], app)
+        if chat_id:
+          try:
+            offset_id = 0
+            limit = 0
+            if len(message.command) > 2:
+              limit = int(message.command[2])
+            if len(message.command) > 3:
+              offset_id = int(message.command[3])
+            for msg in app.iter_history(chat_id, limit=limit, offset_id=offset_id):
+              msg.copy(message.chat.id)
+              sleep(random.randint(1, 10))
+          except Exception as e:
+            message.reply_text(f"```{e}```")
+        else:
+          # reply = message.reply_text("```Invalid Chat Identifier. Give me a chat id, username or message link.```")
+          sleep(5)
+          # reply.delete()
+      else:
+        # reply = message.reply_text("```Invalid Command ! Use /fwd {ChatID} {limit} {FirstMessageID}```")
+        sleep(20)
+        # reply.delete()
 
 app.run()
